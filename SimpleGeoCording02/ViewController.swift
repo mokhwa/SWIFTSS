82f414383fa2cd64ad429d3f493502e0091de309
ybh <2616442@naver.com>
2019-10-14 15:44:42 +0900

    GeoCording

--- SimpleGeoCording02/ViewController.swift
+++ SimpleGeoCording02/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var pins = [MKPointAnnotation]()
+        let addr = "부산 광역시 양정동 429-29 "
+       let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(addr){Placemark, error in
+            
+            if error != nil {
+                print(error!)
+            }
+            if let myPlacemark = Placemark {
+                let myPlacemark = myPlacemark.first
+                print(myPlacemark?.country)
+                print(myPlacemark!.location?.coordinate.latitude)
+                
+                let pin = MKPointAnnotation()
+                pin.coordinate = (myPlacemark?.location!.coordinate)!
+                pin.title = "hd"
+                pin.subtitle = "dd"
+                pins.append(pin)
+                self.mapView.showAnnotations(pins, animated: true)
+                
+            } else {
+                print("nil qkftod")
+            }
+        }
     }
-
 
 }
 
